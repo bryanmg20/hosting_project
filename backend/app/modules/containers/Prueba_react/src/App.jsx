614b17cc84d8a9826ba1dd54a7ec5323d0e543c7
Ky0Rex <sdricardo@uninorte.edu.co>
2025-11-15 22:18:30 -0500

    Nueva plantilla

---
+++ backend/app/modules/containers/Prueba_react/src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div className="app">
+      <h1>React + Vite + Docker</h1>
+      <p>
+        Plantilla básica lista para usar en producción dentro de un contenedor Docker.
+      </p>
+      <ul>
+        <li>Bundler: Vite</li>
+        <li>Vista: React</li>
+        <li>Servidor en producción: Nginx</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
